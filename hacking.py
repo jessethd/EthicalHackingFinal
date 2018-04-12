b0b1ea71e58cb48064d52d6ec8ac303ff667bc40
root <root@localhost.localdomain>
2018-04-12 16:34:47 -0400

    Fixed terminal freezing, and added way to read file

--- hacking.py
+++ hacking.py
@@ -1,22 +1,33 @@
 import subprocess
 import csv
 from threading import Timer
+from os import system
+import time
 
-kill = lambda process: process.kill()
 
-subprocess.call(["airmon-ng", "start", "wlan0"])
+subprocess.call(['airmon-ng', 'start', 'wlan0'])
+p = subprocess.Popen(["airodump-ng", "wlan0mon", "-w", "output", "--output-format", "csv"])
+time.sleep(5)
+p.terminate()
 
-cmd = ['airodump-ng', 'wlan0mon', '-w', 'output', '--output-format', 'csv']
-try:
-	subprocess.run(cmd, timeout=5)
-except subprocess.TimeoutExpired:
-	pass
+#subprocess.call(["airmon-ng", "start", "wlan0"])
+#print('Press Ctrl + C to choose an Access Point')
+#system('airodump-ng wlan0mon -w output --output-format csv')
 
-#subprocess.call(['cat', 'output-01.csv'])
 
-with open("output-01.csv") as f:
-	for line in f:
-		if "CS378-EthicalHacking-GDC-2.212" in line:
-			print (line)
+with open('output-01.csv', newline='') as f:
+    name = ' ' + input("Type in Access Point name: ")
+    for row in f:
+        line = row.split(',')
+        macAddr = ''
+        # If you take in some argument here instead of hardcoded AP name
+        #name = ' luchador69'
+        channel = ''
+        if (name in line):
+            print(line)
+            macAddr = line[0]
+            channel = line[3].replace(" ", "")
+        print(macAddr)
+        print(channel)
 
 
