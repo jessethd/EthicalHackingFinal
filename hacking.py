4a56af53c2cb9f50547f0d2009e453ea81d28430
root <root@localhost.localdomain>
2018-04-12 19:37:34 -0400

    starting access point hosting

--- hacking.py
+++ hacking.py
@@ -7,27 +7,28 @@ import time
 
 subprocess.call(['airmon-ng', 'start', 'wlan0'])
 p = subprocess.Popen(["airodump-ng", "wlan0mon", "-w", "output", "--output-format", "csv"])
-time.sleep(5)
+time.sleep(10)
 p.terminate()
 
 #subprocess.call(["airmon-ng", "start", "wlan0"])
 #print('Press Ctrl + C to choose an Access Point')
 #system('airodump-ng wlan0mon -w output --output-format csv')
 
-
+macAddr = ''
+channel = ''
+name = ' ' + input("Type in Access Point name: ")
 with open('output-01.csv', newline='') as f:
-    name = ' ' + input("Type in Access Point name: ")
     for row in f:
         line = row.split(',')
-        macAddr = ''
         # If you take in some argument here instead of hardcoded AP name
-        #name = ' luchador69'
-        channel = ''
         if (name in line):
-            print(line)
+            #print(line)
             macAddr = line[0]
             channel = line[3].replace(" ", "")
-        print(macAddr)
-        print(channel)
+        #print(macAddr)
+        #print(channel)
 
-
+#print(name)
+#print(macAddr)
+#print(channel)
+#subprocess.call(['airbase-ng', '-e', name[1:], '-a', macAddr, 'wlan0mon'])
